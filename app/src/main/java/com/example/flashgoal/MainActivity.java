2255aad15c9897b2a37e9269334e037d9d71ff5f
BlackTavish <kubalos98@gmail.com>
2020-11-20 17:49:50 +0100

    Added two Activities: LaLiga and PremierLeague and we can go both of them.

--- app/src/main/java/com/example/flashgoal/MainActivity.java
+++ app/src/main/java/com/example/flashgoal/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.flashgoal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView logo_PL,logo_LL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo_PL = findViewById(R.id.PremierLeague);
+        logo_PL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPremierLeague();
+            }
+        });
+
+        logo_LL = findViewById(R.id.LaLiga);
+        logo_LL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLaLiga();
+            }
+        });
+    }
+    public void openPremierLeague(){
+        Intent intent = new Intent(this,PremierLeague.class);
+        startActivity(intent);
+    }
+    public void openLaLiga(){
+        Intent intent = new Intent(this,LaLiga.class);
+        startActivity(intent);
     }
 }
