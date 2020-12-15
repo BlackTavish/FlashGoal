c05d23292b3e47d2b2f2e473c62f6acdfcc17b2f
BlackTavish <kubalos98@gmail.com>
2020-12-15 21:39:29 +0100

    Added: CardView,RecyclerView,parsing with JSON

--- app/src/main/java/com/example/flashgoal/LaLiga.java
+++ app/src/main/java/com/example/flashgoal/LaLiga.java
@@ -1,16 +1,71 @@
 package com.example.flashgoal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class LaLiga extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private ExampleAdapter mExampleAdapter;
+    private ArrayList<ExampleItem> mExampleList;
+    private RequestQueue mRequestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_la_liga);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mExampleList = new ArrayList<>();
+        mRequestQueue = Volley.newRequestQueue(this);
+        parseJSON();
+
+    }
+    private void parseJSON() {
+        String url = "https://pixabay.com/api/?key=19540727-f31919c5419b86947e5a6a270&q=kitten&image_type=photo&pretty=true";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("hits");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject hit = jsonArray.getJSONObject(i);
+                                String creatorName = hit.getString("user");
+                                String imageUrl = hit.getString("webformatURL");
+                                int likeCount = hit.getInt("likes");
+                                mExampleList.add(new ExampleItem(imageUrl, creatorName, likeCount));
+                            }
+                            mExampleAdapter = new ExampleAdapter(LaLiga.this, mExampleList);
+                            mRecyclerView.setAdapter(mExampleAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mRequestQueue.add(request);
     }
 }
